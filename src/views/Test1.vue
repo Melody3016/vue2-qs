b44622d28742ed9e53682abb9726da9e69fb360e
xuanlvzeng <xuanlvzeng@gmail.com>
2021-09-01 22:25:40 +0800

    'v1'

---
+++ src/views/Test1.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="container">
+    <button @click="clickHandle">改变</button>
+    <div class="box">
+      haha
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Test1',
+  data() {
+    return {
+      msg: {
+        a: 1
+      }
+    }
+  },
+  watch: {
+    msg: {
+      handler(newVal, oldVal) {
+        console.log(newVal, oldVal)
+      },
+      deep: true
+    }
+  },
+  methods: {
+    clickHandle() {
+      this.msg.a = 2
+      // this.msg.b = 3
+      this.$set(this.msg, 'b', 3)
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+  .box {
+    min-width: 100px;
+  }
+</style>
