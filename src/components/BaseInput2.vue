8ea511759220aef27b3486eb880377cc3a4d2d8a
xuanlvzeng <xuanlvzeng@gmail.com>
2021-09-08 16:47:05 +0800

    自定义事件名测试

---
+++ src/components/BaseInput2.vue
@@ -0,0 +1,26 @@
+<template>
+   <input type="text"
+    v-model="inputValue"
+    @keyup.enter="enterHandler"
+   />
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      inputValue: ''
+    }
+  },
+  methods: {
+    enterHandler() {
+      this.$emit('my-input', this.inputValue)
+      this.inputValue = ''
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -41,6 +41,14 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/Test5.vue')
+  },
+  {
+    path: '/test6',
+    name: 'Test6',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/Test6.vue')
   }
 ]
 
