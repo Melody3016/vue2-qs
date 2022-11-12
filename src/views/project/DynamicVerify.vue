cbe803a253d90b0cefe78de79fae7197dbb8b5cd
xuanlvzeng <xuanlvzeng@gmail.com>
2022-11-13 00:23:23 +0800

    表单动态校验完善

--- src/views/project/DynamicVerify.vue
+++ src/views/project/DynamicVerify.vue
@@ -4,6 +4,19 @@
       <el-form-item label="name" prop="name">
         <el-input type="text" v-model="formData.name" />
       </el-form-item>
+      <el-form-item label="file" prop="file">
+        <el-input type="file" v-model="formData.file" @change="changeHandler" />
+      </el-form-item>
+      <el-form-item label="select" prop="selectObj.selectVal">
+        <el-select v-model="formData.selectObj.selectVal" placeholder="请选择">
+          <el-option
+            v-for="item in options"
+            :key="item.value"
+            :label="item.label"
+            :value="item.value">
+          </el-option>
+        </el-select>
+      </el-form-item>
       <div class="box"
         v-for="(item,index) in formData.inputList"
         :key="index"
@@ -25,8 +38,28 @@
 export default {
   data() {
     return {
+      options: [{
+        value: '选项1',
+        label: '黄金糕'
+      }, {
+        value: '选项2',
+        label: '双皮奶'
+      }, {
+        value: '选项3',
+        label: '蚵仔煎'
+      }, {
+        value: '选项4',
+        label: '龙须面'
+      }, {
+        value: '选项5',
+        label: '北京烤鸭'
+      }],
       formData: {
         name: '',
+        file: '',
+        selectObj: {
+          selectVal: ''
+        },
         inputList: [
           { key: '', url: '' }
         ]
@@ -35,16 +68,25 @@ export default {
         name: [
           { required: true, message: '请输入name', trigger: 'change' }
         ],
+        file: [
+          { required: true, message: '请选择file', trigger: 'change' }
+        ],
         key: [
           { required: true, message: '请输入Key', trigger: 'change' }
         ],
         url: [
           { required: true, message: '请输入url', trigger: 'change' }
+        ],
+        'selectObj.selectVal': [
+          { required: true, message: '请选择select', trigger: 'change' }
         ]
       }
     }
   },
   methods: {
+    changeHandler(file) {
+      console.log(file, 'file')
+    },
     add() {
       this.formData.inputList.push({})
     },
@@ -59,6 +101,19 @@ export default {
       })
     }
   }
+  // mounted() {
+  //   setTimeout(() => {
+  //     this.formData =  {
+  //       name: '',
+  //       selectObj: {
+  //         selectVal: ''
+  //       },
+  //       inputList: [
+  //         { key: '', url: '' }
+  //       ]
+  //     }
+  //   }, 1000)
+  // }
 }
 </script>
 
